e011e866db9c2dba2bb860476252675cdbc70be9
George Cowie <gecocowie@live.com>
2019-11-29 14:22:16 +0100

    kkkkk

---
+++ orbit_plot.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.loadtxt("Orbit.txt")
+sorted_data = np.transpose(data)
+
+x = sorted_data[0]
+y = sorted_data[1]
+time = sorted_data[2]
+kin_en = sorted_data[3]
+pot_en = sorted_data[4]
+
+plt.figure(1); plt.grid()
+plt.title("Orbit in %.1f year" % (time[-1]))
+plt.plot(0,0,"o",label="Sun",color="yellow")
+plt.plot(x[0],y[0],"o",label="Earth",color="blue")
+plt.plot(x,y,label="Earth orbit",color="blue")
+
+plt.legend()
+
+plt.figure(2); plt.grid()
+plt.plot(time,kin_en,label="Kinetic energy")
+plt.plot(time,pot_en,label="Potential energy")
+plt.plot(time,kin_en+pot_en,label="Total energy")
+plt.legend()
+plt.show()
---
+++ solar_system.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <armadillo>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+//#include "mpi.h"
+
+//For debugging:
+// compile with: g++ solar_system.cpp -o main1 -larmadillo -llapack -lblas
+// execute with ./main1
+
+//For paralellization
+//mpicxx  -o main_mpi.x  main.cpp -std=c++11
+//mpiexec -n 2 ./main_mpi.x
+
+std::ofstream ofile
+double const pi = 3.14159265359;
+double const M_sun = 2*pow(10,30);        // in kilograms
+double const M_earth = 6*pow(10,24);      // in kilograms
+double const sun_rad = 0.00465047;        // in AU
+
+
+void planet(arma::Col <double> &x, arma::Col <double> &y, arma::Col <double> &vx,
+            arma::Col <double> &vy, std::string method){
+  double a,r,ax,ay;
+  double GM = 4*pi*pi;
+
+  int n = x.n_elem;
+
+  arma::Col <double> t = arma::vec(n);
+  double tmin = 0;
+  double tmax = 10;
+  double dt = (tmax-tmin)/n;
+  for (int i=0; i<n; i++){
+    t(i)=i*dt;
+  }
+
+  double red_earth = M_earth/M_sun;
+  //set up vectors containing potential and kinetic energy
+  arma::Col <double> kin_en = arma::vec(n); kin_en(0) = 0.5*red_earth*(vx(0)*vx(0)+vy(0)*vy(0));
+  arma::Col <double> pot_en = arma::vec(n); pot_en(0) = -GM*red_earth/sqrt(x(0)*x(0)+y(0)*y(0));
+
+  if (method == 'euler'){
+    //Solving ode using forward euler
+    for (int i=1; i<n; i++){
+      r = sqrt(x(i-1)*x(i-1) + y(i-1)*y(i-1));
+      a = GM / (r*r);
+      ax = -a*x(i-1);
+      ay = -a*y(i-1);
+      vx(i) = vx(i-1) + ax*dt;
+      vy(i) = vy(i-1) + ay*dt;
+      x(i) = x(i-1) + vx(i-1)*dt;
+      y(i) = y(i-1) + vy(i-1)*dt;
+    }
+    //write results to file for analysis
+    std::string fileout = "Orbit_euler.txt";
+    ofile.open(fileout);
+    ofile << std::setiosflags(std::ios::showpoint | std::ios::uppercase);
+    for (int i=0; i<n; i++){
+      ofile << std::setw(15) << x(i);
+      ofile << std::setw(15) << y(i);
+      ofile << std::setw(15) << t(i);
+      ofile << std::setw(15) << kin_en(i);
+      ofile << std::setw(15) << pot_en(i) << "\n";
+    }
+  }
+
+  else if (method == 'verlet'){
+    //Solving ode using forward euler
+    for (int i=1; i<n; i++){
+      r = sqrt(x(i-1)*x(i-1) + y(i-1)*y(i-1));
+      a = GM / (r*r);
+      ax = -a*x(i-1);
+      ay = -a*y(i-1);
+      vx(i) = vx(i-1) + ax*dt;
+      vy(i) = vy(i-1) + ay*dt;
+      x(i) = x(i-1) + vx(i-1)*dt;
+      y(i) = y(i-1) + vy(i-1)*dt;
+    }
+    //write results to file for analysis
+    std::string fileout = "Orbit_verlet.txt";
+    ofile.open(fileout);
+    ofile << std::setiosflags(std::ios::showpoint | std::ios::uppercase);
+    for (int i=0; i<n; i++){
+      ofile << std::setw(15) << x(i);
+      ofile << std::setw(15) << y(i);
+      ofile << std::setw(15) << t(i);
+      ofile << std::setw(15) << kin_en(i);
+      ofile << std::setw(15) << pot_en(i) << "\n";
+    }
+  }
+  else{
+    std::cout << "You must choose either (euler) or (verlet)\n";
+    exit(1);
+    }
+  ofile.close();
+
+
+////DFGDF
+
+
+} // end of function planet()
+
+
+
+
+int main(int argc, char* argv[]){
+  int len = 10000;
+  std::string method = argv[1];
+  arma::Col <double> x = arma::vec(len); x(0)=1;
+  arma::Col <double> y = arma::vec(len); y(0)=0;
+  arma::Col <double> vx = arma::vec(len); vx(0)=0;
+  arma::Col <double> vy = arma::vec(len); vy(0)=8;
+
+
+  planet(x,y,vx,vy,method);
+
+
+
+
+  return 0;
+} // end of function main()
