6a5c5453d1bc8b6a2a3fd593c8fe8e9420b81d89
Jonas <Jonastf@student.matnat.uio.no>
2019-11-29 16:31:10 +0100

    Something

--- orbit_plot.py
+++ orbit_plot.py
@@ -13,25 +13,32 @@ def plot(file,method):
     pot_en = sorted_data[4]
 
     if method == "euler":
+        col = "blue"
+        meth = "Euler"
+    elif method == "verlet":
         col = "red"
+        meth = "Verlet"
 
-    plt.figure(1); plt.grid()
+
+    plt.figure(1)
     plt.title("Orbit in %.1f year" % (time[-1]))
-    plt.plot(0,0,"o",label="Sun",color="yellow")
-    plt.plot(x[0],y[0],"o",label="Earth",color="blue")
-    plt.plot(x,y,label="Earth orbit",color="blue")
+    plt.plot(x[0],y[0],"o",label="Earth (%s)"%meth,color=col)
+    plt.plot(x,y,label="Earth orbit (%s)"%meth,color=col)
     plt.xlabel("x (AU)"); plt.ylabel("y (AU)")
     plt.legend()
 
-    plt.figure(2); plt.grid()
-    plt.plot(time,kin_en,label="Kinetic energy")
-    plt.plot(time,pot_en,label="Potential energy")
-    plt.plot(time,kin_en+pot_en,label="Total energy")
+    plt.figure(2)
+    plt.plot(time,kin_en,label="Kinetic energy (%s)"%meth)
+    plt.plot(time,pot_en,label="Potential energy (%s)"%meth)
+    plt.plot(time,kin_en+pot_en,label="Total energy (%s)"%meth)
     plt.xlabel("t (year)"); plt.ylabel("Energy")
     plt.legend()
 
 
-euler_plot = plot("Orbit_euler.txt","euler")
+#euler_plot = plot("Orbit_euler.txt","euler")
 verlet_plot = plot("Orbit_verlet.txt","verlet")
 
+plt.figure(1); plt.grid()
+plt.plot(0,0,"o",label="Sun",color="yellow")
+plt.figure(2); plt.grid()
 plt.show()
--- solar_system.cpp
+++ solar_system.cpp
@@ -31,7 +31,7 @@ void planet(arma::Col <double> &x, arma::Col <double> &y, arma::Col <double> &vx
 
   arma::Col <double> t = arma::vec(n);
   double tmin = 0;
-  double tmax = 10;
+  double tmax = 5;
   double dt = (tmax-tmin)/n;
   for (int i=0; i<n; i++){
     t(i)=i*dt;
@@ -43,7 +43,7 @@ void planet(arma::Col <double> &x, arma::Col <double> &y, arma::Col <double> &vx
   arma::Col <double> pot_en = arma::vec(n); pot_en(0) = -GM*red_earth/sqrt(x(0)*x(0)+y(0)*y(0));
 
   if (method == "euler"){
-    //Solving ode using forward euler
+    //Solving ode using forward Euler
     for (int i=1; i<n; i++){
       r = sqrt(x(i-1)*x(i-1) + y(i-1)*y(i-1));
       a = GM / (r*r);
@@ -71,24 +71,44 @@ void planet(arma::Col <double> &x, arma::Col <double> &y, arma::Col <double> &vx
   }
 
   else if (method == "verlet"){
-    //Solving ode using forward euler
+    //Solving ode using velocity Verlet
     double ax_new, ay_new;
     double ax_prev,ay_prev;
+    double vx_half,vy_half;
     double r0 = sqrt(x(0)*x(0) + y(0)*y(0));
     double a0 = GM / (r0*r0);
     ax_prev = -a0*x(0);
     ay_prev = -a0*y(0);
-    for (int i=1; i<n; i++){
-      r = sqrt(x(i-1)*x(i-1) + y(i-1)*y(i-1));
-      a = GM / (r*r);
-      ax_new = -a*x(i-1);
-      ay_new = -a*y(i-1);
-      vx(i) = vx(i-1)+dt/2*(ax_new + ax_prev);
-      vy(i) = vy(i-1)+dt/2*(ay_new + ay_prev);
-      x(i) = x(i-1) + dt*vx(i-1) + dt*dt*ax_prev/2;
-      y(i) = y(i-1) + dt*vy(i-1) + dt*dt*ay_prev/2;
+    for (int i=0; i<n-1; i++){
+      vx_half = vx(i) + 0.5*ax_prev*dt;
+      vy_half = vy(i) + 0.5*ay_prev*dt;
+
+      x(i+1) = x(i) + vx_half*dt;
+      y(i+1) = y(i) + vy_half*dt;
+
+      r = sqrt(x(i+1)*x(i+1) + y(i+1)*y(i+1));
+      ax_new = -GM*x(i)/pow(r,3);
+      ay_new = -GM*y(i)/pow(r,3);
+
+      vx(i+1) = vx_half + 0.5*ax_new*dt;
+      vy(i+1) = vy_half + 0.5*ay_new*dt;
+
       ax_prev = ax_new;
       ay_prev = ay_new;
+
+      /*
+      ax_new = -a*x(i-1);
+      ay_new = -a*y(i-1);
+
+      x(i) = x(i-1) + dt*vx(i-1) + dt*dt*ax_prev/2;
+      y(i) = y(i-1) + dt*vy(i-1) + dt*dt*ay_prev/2;
+
+      vx(i) = vx(i-1)+dt/2*(ax_new + ax_prev);
+      vy(i) = vy(i-1)+dt/2*(ay_new + ay_prev);
+
+      ax_prev = ax_new;
+      ay_prev = ay_new;
+      */
 
       kin_en(i) = 0.5*red_earth*(vx(i)*vx(i)+vy(i)*vy(i));
       pot_en(i) = -GM*red_earth/sqrt(x(i)*x(i)+y(i)*y(i));
@@ -110,23 +130,18 @@ void planet(arma::Col <double> &x, arma::Col <double> &y, arma::Col <double> &vx
     exit(1);
     }
   ofile.close();
-
-
-////DFGDF
-
-
 } // end of function planet()
 
 
 
 
 int main(int argc, char* argv[]){
-  int len = 10000;
+  int len = 100000;
   std::string method = argv[1];
   arma::Col <double> x = arma::vec(len); x(0)=1;
   arma::Col <double> y = arma::vec(len); y(0)=0;
   arma::Col <double> vx = arma::vec(len); vx(0)=0;
-  arma::Col <double> vy = arma::vec(len); vy(0)=8;
+  arma::Col <double> vy = arma::vec(len); vy(0)=5;//sqrt(4*pi*pi);
 
 
   planet(x,y,vx,vy,method);
