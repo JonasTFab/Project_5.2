28cbbefe32752328ca036ddba9bcb693f26d1403
Jonas <Jonastf@student.matnat.uio.no>
2019-12-02 17:40:53 +0100

    3D orbit

---
+++ 3D_testing/3D_orbit_plot.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def plot(file,method):
+    data = np.loadtxt(file)
+    sorted_data = np.transpose(data)
+
+    x = sorted_data[0]
+    y = sorted_data[1]
+    z = sorted_data[2]
+    time = sorted_data[3]
+    kin_en = sorted_data[4]
+    pot_en = sorted_data[5]
+
+    if method == "euler":
+        col = "black"
+        meth = "Euler"
+    elif method == "verlet":
+        col = "red"
+        meth = "Verlet"
+
+
+    ax.text2D(0.4,1,"Orbit in %.1f year" % (time[-1]),transform=ax.transAxes)
+    ax.scatter(x[0],y[0],z[0],"o",label="Earth (%s)"%meth,color=col)
+    ax.plot(x,y,z,label="Earth path (%s)"%meth,color=col)
+    ax.set_xlabel("x (AU)"); ax.set_ylabel("y (AU)"); ax.set_zlabel("z (AU)")
+
+    ax2.plot(time,kin_en,"--",label="Kinetic energy (%s)"%meth,color=col)
+    ax2.plot(time,pot_en,"-.",label="Potential energy (%s)"%meth,color=col)
+    ax2.plot(time,kin_en+pot_en,label="Total energy (%s)"%meth,color=col)
+    ax2.set_xlabel("t (year)"); ax2.set_ylabel("Energy")
+
+
+fig = plt.figure(1)
+ax = fig.gca(projection="3d")
+fig2 = plt.figure(2)
+ax2 = fig2.add_subplot(111); ax2.grid()
+euler_plot = plot("Orbit_euler.txt","euler")
+verlet_plot = plot("Orbit_verlet.txt","verlet")
+
+ax.scatter(0,0,0,"O",label="Sun",color="orange")
+ax.legend(); ax2.legend()
+plt.show()
--- orbit_plot.py
+++ orbit_plot.py
@@ -1,30 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-<<<<<<< HEAD
-data = np.loadtxt("Orbit_verlet.txt")
-sorted_data = np.transpose(data)
-
-x = sorted_data[0]
-y = sorted_data[1]
-time = sorted_data[2]
-kin_en = sorted_data[3]
-pot_en = sorted_data[4]
-
-plt.figure(1); plt.grid()
-plt.title("Orbit in %.1f year" % (time[-1]))
-plt.plot(0,0,"o",label="Sun",color="yellow")
-plt.plot(x[0],y[0],"o",label="Earth",color="blue")
-plt.plot(x,y,label="Earth orbit",color="blue")
-
-plt.legend()
-
-plt.figure(2); plt.grid()
-plt.plot(time,kin_en,label="Kinetic energy")
-plt.plot(time,pot_en,label="Potential energy")
-plt.plot(time,kin_en+pot_en,label="Total energy")
-plt.legend()
-=======
 
 def plot(file,method):
     data = np.loadtxt(file)
@@ -37,7 +13,7 @@ def plot(file,method):
     pot_en = sorted_data[4]
 
     if method == "euler":
-        col = "blue"
+        col = "black"
         meth = "Euler"
     elif method == "verlet":
         col = "red"
@@ -52,21 +28,17 @@ def plot(file,method):
     plt.legend()
 
     plt.figure(2)
-    plt.plot(time,kin_en,label="Kinetic energy (%s)"%meth)
-    plt.plot(time,pot_en,label="Potential energy (%s)"%meth)
-    plt.plot(time,kin_en+pot_en,label="Total energy (%s)"%meth)
+    plt.plot(time,kin_en,"--",label="Kinetic energy (%s)"%meth,color=col)
+    plt.plot(time,pot_en,"-.",label="Potential energy (%s)"%meth,color=col)
+    plt.plot(time,kin_en+pot_en,label="Total energy (%s)"%meth,color=col)
     plt.xlabel("t (year)"); plt.ylabel("Energy")
     plt.legend()
 
 
-#euler_plot = plot("Orbit_euler.txt","euler")
+euler_plot = plot("Orbit_euler.txt","euler")
 verlet_plot = plot("Orbit_verlet.txt","verlet")
 
-<<<<<<< HEAD
 plt.figure(1); plt.grid()
-plt.plot(0,0,"o",label="Sun",color="yellow")
+plt.plot(0,0,"o",label="Sun",color="yellow"); plt.legend()
 plt.figure(2); plt.grid()
-=======
->>>>>>> 8e8e7c2f49ea2e7a61f163e423d0d266b11bca41
->>>>>>> master
 plt.show()
